014be8e9c6c06cbb37028c99976538b19b7e2fec
ootsuka wakako <wakako.otsuka@rakus-partners.co.jp>
2019-08-28 11:41:18 +0900

    掲示板

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Car {
+	private int speed;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+
+}
